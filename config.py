5898ee8806945cdf26d1c16213755ff2defb660e
ak67civil <taufeeque671@gmail.com>
2026-03-15 17:30:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,11 @@
- import os
+import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 12345)) 
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
-    
-    # MongoDB Link (Heroku Config Vars se aayega)
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "your_hash")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_token")
     MONGO_URL = os.environ.get("MONGO_URL", "")
-    
-    # Log Channel ID Jahan report aayegi
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100123456789))
-
-    # Owner ID (Sirf aap commands use kar sako isliye)
     OWNER_ID = int(os.environ.get("OWNER_ID", 12345678))
+ 
     
