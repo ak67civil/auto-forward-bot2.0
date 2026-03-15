873a20b027d147f4ecf34465d9991f72cb347b12
ak67civil <taufeeque671@gmail.com>
2026-03-15 16:37:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
-import os
+ import os
 
 class Config:
     API_ID = int(os.environ.get("API_ID", 12345)) 
     API_HASH = os.environ.get("API_HASH", "your_api_hash")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
     
-    # List IDs separated by space in Heroku, then split here
-    FROM_CHANNELS = [int(x) if x.startswith("-100") else x for x in os.environ.get("FROM_CHANNELS", "-100123 -100456").split()]
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", -100987654321))
+    # MongoDB Link (Heroku Config Vars se aayega)
+    MONGO_URL = os.environ.get("MONGO_URL", "")
+    
+    # Log Channel ID Jahan report aayegi
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100123456789))
+
+    # Owner ID (Sirf aap commands use kar sako isliye)
+    OWNER_ID = int(os.environ.get("OWNER_ID", 12345678))
     
