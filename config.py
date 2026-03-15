7b22fb906b847140e8ca36b50e8721624376f104
ak67civil <taufeeque671@gmail.com>
2026-03-15 16:16:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,11 @@
 import os
 
 class Config:
-    # Telegram API Credentials
-    API_ID = int(os.environ.get("API_ID", 1234567)) # Apna API ID yahan likhein ya Environment Variable set karein
-    API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token_here")
-
-    # Forwarding Settings
-    # Source Channels: Jahan se message uthana hai (IDs ya Usernames ki list)
-    FROM_CHANNELS = [-100123456789, "source_channel_username"]
+    API_ID = int(os.environ.get("API_ID", 12345)) 
+    API_HASH = os.environ.get("API_HASH", "your_api_hash")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
     
-    # Destination Channel: Jahan forward karna hai
-    TO_CHANNEL = -100987654321
-
-    # Optional: Agar message se purane channel ka link/naam hatana ho
-    REMOVE_CAPTION = True
+    # List IDs separated by space in Heroku, then split here
+    FROM_CHANNELS = [int(x) if x.startswith("-100") else x for x in os.environ.get("FROM_CHANNELS", "-100123 -100456").split()]
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", -100987654321))
+    
